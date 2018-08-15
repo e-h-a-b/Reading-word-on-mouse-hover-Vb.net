1c98092aaf70f2b9b52f521b773296c18b555e30
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-08-15 11:40:02 +0200

    Add files via upload

---
+++ ReadWordsMouseover/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(4, 3)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(639, 329)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(24, 344)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(140, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(24, 370)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(140, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(237, 351)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(237, 377)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(646, 417)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ ReadWordsMouseover/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+ Public curWord
+    Private Sub RichTextBox1_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles RichTextBox1.MouseMove
+         Label1.Text = e.X
+        Label2.Text = e.Y
+        GetWordUnderMouse(Me.RichTextBox1, e.X, e.Y)
+        Timer1.Start()
+    End Sub
+    Public Function GetWordUnderMouse(ByRef Rtf As System.Windows.Forms.RichTextBox, ByVal X As Integer, ByVal Y As Integer) As String
+        On Error Resume Next
+        Dim POINT As System.Drawing.Point = New System.Drawing.Point()
+        Dim Pos As Integer, i As Integer, lStart As Integer, lEnd As Integer
+        Dim lLen As Integer, sTxt As String, sChr As String
+        '
+        POINT.X = X
+        POINT.Y = Y
+        GetWordUnderMouse = vbNullString
+        '
+        With Rtf
+            lLen = Len(.Text)
+            sTxt = .Text
+            Pos = Rtf.GetCharIndexFromPosition(POINT)
+            If Pos > 0 Then
+                For i = Pos To 1 Step -1
+                    sChr = Mid(sTxt, i, 1)
+                    If sChr = " " Or sChr = Chr(13) Or i = 1 Then
+                        'if the starting character is vbcrlf then
+                        'we want to chop that off
+                        If sChr = Chr(13) Then
+                            lStart = (i + 2)
+                        Else
+                            lStart = i
+                        End If
+                        Exit For
+                    End If
+                Next i
+                For i = Pos To lLen
+                    If Mid(sTxt, i, 1) = " " Or Mid(sTxt, i, 1) = Chr(13) Or i = lLen Then
+                        lEnd = i + 1
+                        Exit For
+                    End If
+                Next i
+                If lEnd >= lStart Then
+                    GetWordUnderMouse = Trim(Mid(sTxt, lStart, lEnd - lStart))
+                End If
+            End If
+        End With
+    End Function
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        Dim h As Integer = Label1.Text
+        Dim h2 As Integer = Label2.Text
+        '  MsgBox(GetWordUnderMouse(Me.RichTextBox1, h, h2))
+        TextBox1.Text = GetWordUnderMouse(Me.RichTextBox1, h, h2)
+        TextBox2.Text = GetWordUnderMouse(Me.RichTextBox1, h, h2)
+        Dim sapi
+        sapi = CreateObject("sapi.spvoice")
+        sapi.Speak(GetWordUnderMouse(Me.RichTextBox1, h, h2))
+
+        Dim a As String
+        Dim b As String
+        Dim index_a As Integer
+        Dim index_b As Integer
+        a = TextBox1.Text
+        b = TextBox2.Text
+        index_a = InStr(RichTextBox1.Text, a)
+        index_b = InStr(RichTextBox1.Text, b)
+        If index_a And index_b Then
+            RichTextBox1.Focus()
+            RichTextBox1.SelectionStart = index_a - 1
+            RichTextBox1.SelectionLength = (index_b - index_a) + Len(b)
+        End If
+        Me.Timer1.Enabled = False
+    End Sub
+End Class
